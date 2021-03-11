be2f2d144d60c7608b0127fc1e7f976cd09f2381
Ramanujam3991 <ramanujam3991@gmail.com>
2021-03-12 05:13:21 +0530

    working rough draft

--- movie.js
+++ movie.js
@@ -3,6 +3,7 @@ var row =6;
 var col = 10;
 var current_selected_lst = [];
 var already_selected_lst = [];
+var totalPrice= 0;
 for(var i = 0; i < row; i++)
     $('.seat-map-table').append('<tr class= "ticket-row row'+i+'"></tr>')
 
@@ -23,8 +24,19 @@ $('.ticket-row td').click(function(){
     current_selected_lst.push(className.substr(className.indexOf('td'),4));
     console.log('current list:'+current_selected_lst)
 
-    
-    
+    // Fill the pricing table
+    var moviePrice = $('#movie').find(':selected').val();
+    var seatsSelected = current_selected_lst.length
+    totalPrice = moviePrice*seatsSelected; //global
+    $('.totalPrice').val(totalPrice)
+    $('.pricing-table tr:eq(0) td:eq(1)').text('$'+moviePrice);
+    $('.pricing-table tr:eq(1) td:eq(1)').text(seatsSelected);
+    $('.pricing-table tr:eq(2) td:eq(1)').text('$'+totalPrice);
 
 })
+
+$('.buy-tickets').click(function(){
+    alert('open new tab')
+});
+
 })//End of jQuery
